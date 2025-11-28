35aff1f48d998e47951e06d22352e2818e670897
SAI CHIRAG B <169077463+08SAI@users.noreply.github.com>
2025-11-28 15:23:51 +0530

    Rename index function to hello and update response

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+# app.py
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "ZipGo service online for CIE set 183 - v4\n"
+@app.route('/')
+def hello():
+    return "ZipGo Quick Commerce service running\n"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=12184)
+    app.run(host='0.0.0.0', port=12184)
